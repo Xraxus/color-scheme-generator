cbf92c1a0adf0e5e5dcc60e75da41db464f57101
Kamil Kobylarz <xraxus@gmail.com>
2023-10-28 14:19:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const colorForm = document.querySelector("#color-form");
+
+colorForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const colorInputVal = document.querySelector("#color-input").value.slice(1);
+  const selectedMode = document.getElementById("mode-selector").value;
+
+  colorForm.reset();
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${colorInputVal}&mode=${selectedMode}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const colorPalette = data.colors.map((item) => item.hex.value);
+      const colorElArray = document.querySelectorAll(".color");
+
+      colorElArray.forEach(function (item, index) {
+        item.innerHTML = `<span>${colorPalette[index]}</span>`;
+        item.style.backgroundColor = colorPalette[index];
+      });
+    });
+});
